9b6a7311732c5ef2bd9dd7d8d5f44912593c0593
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-04-22 18:57:26 +0100

    Stopping collabosphere before starting it

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,5 +3,8 @@
 # The directory where forever logs should be sent to
 LOG_DIR=/home/app_collabosphere/log
 
+# Make sure nothing else is running before we start the app server
+./deploy/stop.sh
+
 # Start the app server
 ./node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 start app.js
