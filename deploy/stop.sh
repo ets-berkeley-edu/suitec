12b6d01793685e0b02428262dadbbd4a38c75cfa
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-04-22 18:44:49 +0100

    Use stopall action to stop collabosphere

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -4,4 +4,4 @@
 LOG_DIR=/home/app_collabosphere/log
 
 # Stop the app server
-./node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 stop
+./node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 stopall
