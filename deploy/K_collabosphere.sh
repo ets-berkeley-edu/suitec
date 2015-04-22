0b4780ec82a1dd10747327b3b33549d65e3d2744
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-04-22 18:42:10 +0100

    Moved LOG_DIR into start and stop.sh

--- deploy/K_collabosphere.sh
+++ deploy/K_collabosphere.sh
@@ -2,9 +2,6 @@
 #
 # The stop script for collabosphere. This needs to go into /home/app_collabosphere/init.d/
 
-# The directory where forever logs should be sent to
-LOG_DIR=/home/app_collabosphere/log
-
 # Change directory to where collabosphere should've been installed
 cd /home/app_collabosphere/collabosphere
 
--- deploy/start.sh
+++ deploy/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# The directory where forever logs should be sent to
+LOG_DIR=/home/app_collabosphere/log
+
 # Start the app server
 ./node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 start app.js
