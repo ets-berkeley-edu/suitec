3598c02866dd9640a7ba76813e9372397b7dc49c
Paul Kerschen <kerschen@berkeley.edu>
2016-08-12 22:40:27 -0700

    COL-429 Update comments in migration script

--- scripts/20160812-col429/add_activity_enums.sql
+++ scripts/20160812-col429/add_activity_enums.sql
@@ -1,16 +1,16 @@
--- Add new activity enums
+-- Add new enum values for `type` column in `activities` table
 
 ALTER TYPE enum_activities_type ADD VALUE 'view_asset';
 ALTER TYPE enum_activities_type ADD VALUE 'whiteboard_add_asset';
 ALTER TYPE enum_activities_type ADD VALUE 'whiteboard_chat';
 
--- Add new activity type enums
+-- Add new enum values for `type` column in `activity_types` table
 
 ALTER TYPE enum_activity_types_type ADD VALUE 'view_asset';
 ALTER TYPE enum_activity_types_type ADD VALUE 'whiteboard_add_asset';
 ALTER TYPE enum_activity_types_type ADD VALUE 'whiteboard_chat';
 
--- Add new activity object enums
+-- Add new enum values for `object_type` column in `activities` table
 
 ALTER TYPE enum_activities_object_type ADD VALUE 'chat';
 ALTER TYPE enum_activities_object_type ADD VALUE 'whiteboard';
