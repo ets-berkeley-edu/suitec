b5280c54b52ee4d40ff7b48fd5a3131eea8ad7f4
Paul Kerschen <kerschen@berkeley.edu>
2016-11-10 13:22:58 -0800

    COL-458 Make Canvas assignment categories invisible

--- scripts/20160910-col398/add_category_visible.sql
+++ scripts/20160910-col398/add_category_visible.sql
@@ -2,3 +2,7 @@
 -- should be displaying in the Asset Library
 
 ALTER TABLE categories ADD visible BOOLEAN NOT NULL DEFAULT true;
+
+-- Categories derived from Canvas assignments should be invisible by default.
+
+UPDATE categories SET visible = false WHERE canvas_assignment_id IS NOT NULL;
