c9400fa72075d9a5c78fdb0ae2b627849479ce68
Paul Kerschen <kerschen@berkeley.edu>
2016-11-28 16:00:33 -0800

    COL-514 Use correct working directory in start and stop scripts

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -3,5 +3,8 @@
 # The directory where forever logs should be sent to
 LOG_DIR=~/log
 
+# Go to base SuiteC directory
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
+
 # Stop the app server
 ./node_modules/.bin/forever -a -l "$LOG_DIR/forever.log" -i "$LOG_DIR/forever_app.log" -e "$LOG_DIR/forever_app.log" -m 10 stopall
