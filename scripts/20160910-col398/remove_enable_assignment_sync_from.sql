fa79daa1afefb6a96baf069921aea5020ecdc094
Paul Kerschen <kerschen@berkeley.edu>
2016-12-14 13:24:09 -0800

    COL-580 Add rollback SQL for v1.7 migrations

--- scripts/20160910-col398/remove_enable_assignment_sync_from.sql
+++ scripts/20160910-col398/remove_enable_assignment_sync_from.sql
@@ -1,3 +1,10 @@
--- Remove the per-course assignment synching setting
+-- Remove the per-course assignment sync setting
 
 ALTER TABLE courses DROP enable_assignment_sync_from;
+
+/**** ROLLBACK ****
+
+-- This will recreate the dropped setting with default NULL (i.e. not synced.) Syncing must be enabled manually
+-- for any courses that need it.
+
+ALTER TABLE courses ADD enable_assignment_sync_from TIMESTAMP WITH TIME ZONE;
